73d9cb41b0b2c3da8b34f116fb3dfd8bb08c78b4
yngridAMatos <matosingridy76@gmail.com>
2025-05-05 13:46:39 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,39 @@
+//especificando as variáveis do html
+let prevButton = document.getElementById('previous')
+let nextButton = document.getElementById('next')
+let container = document.querySelector('.container')
+let items = container.querySelectorAll('.list .item')
+let indicator = document.querySelector('.indicators')
+let dots = indicator.querySelectorAll('ul li')
+let list = container.querySelector('.list')
+
+let active = 0
+let firstPosition = 0
+let lastPosition = items.length - 1
+
+function setSlider(){
+    let itemOld = container.querySelector('.list .item.active')
+    itemOld.classList.remove('active')
+
+    let dotsOld = indicator.querySelector('ul li.active')
+    dotsOld.classList.remove('active')
+
+    dots[active].classList.add('active')
+    indicator.querySelector('.number').innerHTML = '0' +(active + 1)
+}
+
+nextButton.onclick = () => {
+    list.style.setProperty('--calculation', 1)
+    //Mesma coisa que if e else:
+    active = active + 1 > lastPosition ? 0 : active + 1
+    setSlider()
+    items[active].classList.add('active')
+}
+
+prevButton.onclick = () => {
+    list.style.setProperty('--calculation', -1)
+    //Voltando, mesma lógica porém algumas alterações:
+    active = active - 1 < firstPosition ? lastPosition : active - 1
+    setSlider()
+    items[active].classList.add('active')
+}
